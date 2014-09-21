c6ffb68256937eed2b0e4dfa869dc4aab8c02ed0
Kritin Vij <kritinvij@gmail.com>
2014-09-21 18:56:48 +0530

    js patch

--- home.js
+++ home.js
@@ -1,3 +1,16 @@
 window.onload = function() {
-	alert("yo");
+	var navEl = document.getElementsByClassName("navElements");
+	
+	for (var i = 0; i < navEl.length; i++) {
+		navEl[i].onlick = changeActive;
+	}
+}
+
+function changeActive() {
+	this.removeClassName("navElements");
+	this.addClassName("active");
+	
+	var activeNav = document.getElementsByClassName("active");
+	activeNav[0].addClassName("navElements");
+	activeNav[0].removeClassName("navElements");
 }
