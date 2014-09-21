809269aa8526a786d49a1ecd5498c18e87cd8370
Kritin Vij <kritinvij@gmail.com>
2014-09-21 17:44:37 +0530

    js debugging

--- home.js
+++ home.js
@@ -1,8 +1,10 @@
 window.onload = function() {
 	activeLi();
+	alert("yo");
 }
 
 function activeLi() {
+	alert("yo2");
 	var LIs = document.getElementsByClassName("navElements");
 	for (var i = 0; i < LIs.length; i++) {
 		if (LIs[i].onclick) {
