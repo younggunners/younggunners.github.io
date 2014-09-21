d039487ee56665b953ededbb5744b9953a77acc4
Kritin Vij <kritinvij@gmail.com>
2014-09-21 18:47:18 +0530

    js check

--- home.js
+++ home.js
@@ -1,14 +1,3 @@
 window.onload = function() {
-	activeLi();
 	alert("yo");
-}
-
-function activeLi() {
-	alert("yo2");
-	var LIs = document.getElementsByClassName("navElements");
-	for (var i = 0; i < LIs.length; i++) {
-		if (LIs[i].onclick) {
-			LIs[i].addClassName("active");
-		}
-	}
 }
